1d5dc17870c1990b54c92cd7d275f940ef3af13c
PRiveraA <priveraa94@gmail.com>
2023-03-19 06:20:02 +0000

    Completed jumbotron

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -2,14 +2,18 @@ import React from "react";
 
 export default function Jumbotron() {
   return (
-    <div class="container">
-      <div class="jumbotron bg-body">
-        <h1>Bootstrap Tutorial</h1>
-        <p>
-          Bootstrap is the most popular HTML, CSS, and JS framework for
-          developing responsive, mobile-first projects on the web.
+    <section className="container m-4">
+      <div className="jumbotron bg-light px-4 py-5 rounded">
+        <h1 className="display-1">A Warm Welcome!</h1>
+        <p className="lead">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
+          sit laboriosam impedit nulla tempore ad dolores dolor sequi quos fugit
+          harum at, reiciendis recusandae necessitatibus.
         </p>
+        <button className="btn btn-lg btn-primary">
+          Call to action!
+        </button>
       </div>
-    </div>
+    </section>
   );
 }
