7d7611eaf8a5df3feeb9a40e7230d416b6c8dc8e
PRiveraA <priveraa94@gmail.com>
2023-03-19 05:07:30 +0000

    Initial commit

---
+++ src/js/component/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Jumbotron from "./Jumbotron.jsx";
+
+//create your first component
+const Home = () => {
+	return (
+		<div className="body">
+			<Jumbotron />
+		</div>
+	);
+};
+
+export default Home;
---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Jumbotron() {
+  return (
+    <div class="container">
+      <div class="jumbotron bg-body">
+        <h1>Bootstrap Tutorial</h1>
+        <p>
+          Bootstrap is the most popular HTML, CSS, and JS framework for
+          developing responsive, mobile-first projects on the web.
+        </p>
+      </div>
+      <p>This is some text.</p>
+      <p>This is another text.</p>
+    </div>
+  );
+}
